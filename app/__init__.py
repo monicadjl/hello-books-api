3f6c82aaf06c4d993cf8d512d116f4416884c35f
Monica <monicalagdaan@hotmail.com>
2023-04-29 22:58:50 -0700

    baseline finished

---
+++ app/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+def create_app(test_config=None):
+    app = Flask(__name__)
+
+    return app
